d2654850e443fbc2dcde3e95d65497ac1b6d01d1
Jonas Hals <contact@jonashals.me>
2019-04-03 14:55:12 +0200

    Improve TypeScript usage with classes, optional params and promises

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,4 @@
-import {createRequest, reqData} from './index';
+import {GetRequest, Request, requestWrapper, SendRequest} from './index';
 import {assert} from 'chai';
 import 'mocha';
 
@@ -7,104 +7,104 @@ describe('create request', () => {
         const jobID = "278c97ffadb54a5bbb93cfec5f7b5503";
         const req = {
             id: jobID,
-            data: <reqData>{}
+            data: <Request>{}
         };
         const timeout = 5000;
 
         it('should fail on invalid method', (done) => {
             // Notice method not set.
-            createRequest(req, (statusCode, data) => {
-                assert.equal(statusCode, 400, "status code");
-                assert.equal(data.jobRunID, jobID, "job id");
-                assert.isUndefined(data.data, "response data");
+            requestWrapper(req).then((response) => {
+                assert.equal(response.statusCode, 400, "status code");
+                assert.equal(response.jobRunID, jobID, "job id");
+                assert.isUndefined(response.data, "response data");
                 done();
-            })
+            });
         });
 
         let payoutId = "";
 
         it('should send payment/payout', (done) => {
-            req.data = <reqData>{
+            req.data = <SendRequest>{
                 method: "sendPayout",
                 amount: process.env.TEST_AMOUNT || 10,
                 currency: process.env.TEST_CURRENCY || "USD",
                 receiver: process.env.TEST_RECEIVER || "your-buyer@example.com"
             };
-            createRequest(req, (statusCode, data) => {
-                assert.equal(statusCode, 201, "status code");
-                assert.equal(data.jobRunID, jobID, "job id");
-                assert.isNotEmpty(data.data, "response data");
-                assert.isNotEmpty(data.data.result, "payout id");
-                payoutId = data.data.batch_header.payout_batch_id;
+            requestWrapper(req).then((response) => {
+                assert.equal(response.statusCode, 201, "status code");
+                assert.equal(response.jobRunID, jobID, "job id");
+                assert.isNotEmpty(response.data, "response data");
+                assert.isNotEmpty(response.data.result, "payout id");
+                payoutId = response.data.batch_header.payout_batch_id;
                 done();
-            })
+            });
         }).timeout(timeout);
 
         it('should get payout details', (done) => {
-            req.data = <reqData>{
+            req.data = <GetRequest>{
                 method: "getPayout",
                 payout_id: process.env.TEST_PAYOUT_ID || payoutId
             };
-            createRequest(req, (statusCode, data) => {
-                assert.equal(statusCode, 200, "status code");
-                assert.equal(data.jobRunID, jobID, "job id");
-                assert.isNotEmpty(data.data, "response data");
-                assert.isNotEmpty(data.data.result, "payout id");
+            requestWrapper(req).then((response) => {
+                assert.equal(response.statusCode, 200, "status code");
+                assert.equal(response.jobRunID, jobID, "job id");
+                assert.isNotEmpty(response.data, "response data");
+                assert.isNotEmpty(response.data.result, "payout id");
                 done();
-            })
+            });
         }).timeout(timeout);
 
         it('should get payout details using ENV variable', (done) => {
             process.env.API_METHOD = "getPayout";
-            req.data = <reqData>{
+            req.data = <Request>{
                 method: "sendPayout",
                 payout_id: process.env.TEST_PAYOUT_ID || payoutId
             };
-            createRequest(req, (statusCode, data) => {
-                assert.equal(statusCode, 200, "status code");
-                assert.equal(data.jobRunID, jobID, "job id");
-                assert.isNotEmpty(data.data, "response data");
-                assert.isNotEmpty(data.data.result, "payout id");
+            requestWrapper(req).then((response) => {
+                assert.equal(response.statusCode, 200, "status code");
+                assert.equal(response.jobRunID, jobID, "job id");
+                assert.isNotEmpty(response.data, "response data");
+                assert.isNotEmpty(response.data.result, "payout id");
                 done();
-            })
+            });
         }).timeout(timeout);
 
         it('should fail sendPayout with missing amount', (done) => {
-            req.data = <reqData>{
+            req.data = <SendRequest>{
                 method: "sendPayout",
                 receiver: "your-buyer@example.com"
             };
-            createRequest(req, (statusCode, data) => {
-                assert.equal(statusCode, 400, "status code");
-                assert.equal(data.jobRunID, jobID, "job id");
-                assert.isUndefined(data.data, "response data");
+            requestWrapper(req).then((response) => {
+                assert.equal(response.statusCode, 400, "status code");
+                assert.equal(response.jobRunID, jobID, "job id");
+                assert.isUndefined(response.data, "response data");
                 done();
-            })
+            });
         }).timeout(timeout);
 
         it('should fail sendPayout with missing receiver', (done) => {
-            req.data = <reqData>{
+            req.data = <Request>{
                 method: "sendPayout",
                 amount: 10
             };
-            createRequest(req, (statusCode, data) => {
-                assert.equal(statusCode, 400, "status code");
-                assert.equal(data.jobRunID, jobID, "job id");
-                assert.isUndefined(data.data, "response data");
+            requestWrapper(req).then((response) => {
+                assert.equal(response.statusCode, 400, "status code");
+                assert.equal(response.jobRunID, jobID, "job id");
+                assert.isUndefined(response.data, "response data");
                 done();
-            })
+            });
         }).timeout(timeout);
 
         it('should fail getPayout with missing payout id', (done) => {
-            req.data = <reqData>{
+            req.data = <GetRequest>{
                 method: "getPayout"
             };
-            createRequest(req, (statusCode, data) => {
-                assert.equal(statusCode, 400, "status code");
-                assert.equal(data.jobRunID, jobID, "job id");
-                assert.isUndefined(data.data, "response data");
+            requestWrapper(req).then((response) => {
+                assert.equal(response.statusCode, 400, "status code");
+                assert.equal(response.jobRunID, jobID, "job id");
+                assert.isUndefined(response.data, "response data");
                 done();
-            })
+            });
         }).timeout(timeout);
     })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 import * as paypal from "paypal-rest-sdk";
 
-export type reqData = {
-    method: string | undefined,
-    amount: number | string | undefined,
-    currency: string | undefined,
-    receiver: string | undefined,
-    payout_id: string | undefined,
-    email_subject: string | undefined,
-    email_message: string | undefined,
-    recipient_type: string | undefined,
-    note: string | undefined,
-    sender_item_id: string | undefined,
-    type: string | undefined,
+class Response {
+    jobRunID: string;
+    statusCode: number;
+    status?: string;
+    data?: any;
+    error?: any;
+}
+
+export class Request {
+    method?: string;
+}
+
+export class GetRequest extends Request {
+    payout_id: string;
+    type?: string;
+}
+
+export class SendRequest extends Request {
+    amount: string;
+    receiver: string;
+    currency?: string;
+    recipient_type?: string;
+    note?: string;
+    sender_item_id?: string;
+    email_subject?: string;
+    email_message?: string
 }
 
 paypal.configure({
@@ -20,107 +34,111 @@ paypal.configure({
     "client_secret": process.env.CLIENT_SECRET
 });
 
-const sendPayout = (data: reqData, callback: { (statusCode: number, msg: string): void }) => {
-    if (!('amount' in data) || !('receiver' in data)) {
-        return callback(400, "missing required parameters")
-    }
-
-    let sender_batch_id = Math.random().toString(36).substring(9);
-    let payoutItem = {
-        "sender_batch_header": {
-            "sender_batch_id": sender_batch_id,
-            "email_subject": data.email_subject || "",
-            "email_message": data.email_message || "",
-        },
-        "items": [
-            {
-                "recipient_type": data.recipient_type || "EMAIL",
-                "amount": {
-                    "value": data.amount,
-                    "currency": data.currency || "USD"
-                },
-                "receiver": data.receiver,
-                "note": data.note || "",
-                "sender_item_id": data.sender_item_id || ""
-            }
-        ]
-    };
-
-    paypal.payout.create(payoutItem, true, (error: any, payout: any) => {
-        if (error) return callback(error.httpStatusCode, error);
-        return callback(payout.httpStatusCode, payout)
-    })
-};
-
-const getPayout = (data: reqData, callback: { (statusCode: number, msg: any): void }) => {
-    if (!('payout_id' in data)) {
-        return callback(400, "missing required parameters")
-    }
-
-    let type = data.type || "batch";
-    let request;
-    switch (type.toLowerCase()) {
-        case "item":
-            request = paypal.payoutItem;
-            break;
-        case "batch":
-            request = paypal.payout;
-            break;
-        default:
-            return callback(400, "invalid method")
-    }
-
-    request.get(data.payout_id, (error: any, payout: any) => {
-        if (error) return callback(error.httpStatusCode, error);
-        return callback(payout.httpStatusCode, payout);
-    })
-};
-
-export const createRequest = (input: any = {}, callback: { (statusCode: number, data: any): void } = () => {
-}) => {
-    let action;
-    const data = <reqData>input.data;
-    const method = process.env.API_METHOD || data.method || "";
-    switch (method.toLowerCase()) {
-        case "sendpayout":
-            action = sendPayout;
-            break;
-        case "getpayout":
-            action = getPayout;
-            break;
-        default:
-            return callback(400, {
-                jobRunID: input.id,
-                status: "errored",
-                error: "Invalid method",
-                statusCode: 400
-            })
-    }
-
-    action(data, (statusCode, data) => {
-        if (statusCode < 200 || statusCode >= 300) {
-            return callback(statusCode, {
-                jobRunID: input.id,
-                status: "errored",
-                error: data,
-                statusCode: statusCode
-            })
+const sendPayout = async (data: SendRequest) => {
+    return new Promise(((resolve, reject) => {
+        if (!('amount' in data) || !('receiver' in data)) {
+            return reject({statusCode: 400, data: "missing required parameters"});
         }
 
-        data.result = data.batch_header.payout_batch_id || "";
-        return callback(statusCode, {
-            jobRunID: input.id,
-            data: data,
-            statusCode: statusCode
+        let sender_batch_id = Math.random().toString(36).substring(9);
+        let payoutItem = {
+            "sender_batch_header": {
+                "sender_batch_id": sender_batch_id,
+                "email_subject": data.email_subject || "",
+                "email_message": data.email_message || "",
+            },
+            "items": [
+                {
+                    "recipient_type": data.recipient_type || "EMAIL",
+                    "amount": {
+                        "value": data.amount,
+                        "currency": data.currency || "USD"
+                    },
+                    "receiver": data.receiver,
+                    "note": data.note || "",
+                    "sender_item_id": data.sender_item_id || ""
+                }
+            ]
+        };
+
+        paypal.payout.create(payoutItem, true, (error: any, payout: any) => {
+            if (error) return reject({statusCode: error.httpStatusCode, data: error});
+            return resolve({statusCode: payout.httpStatusCode, data: payout});
         })
+    }))
+};
+
+const getPayout = async (data: GetRequest) => {
+    return new Promise(((resolve, reject) => {
+        if (!('payout_id' in data))
+            return reject({statusCode: 400, data: "missing required parameters"});
+
+        let type = data.type || "batch";
+        let request;
+        switch (type.toLowerCase()) {
+            case "item":
+                request = paypal.payoutItem;
+                break;
+            case "batch":
+                request = paypal.payout;
+                break;
+            default:
+                return reject({statusCode: 400, data: "invalid method"});
+        }
+
+        request.get(data.payout_id, (error: any, payout: any) => {
+            if (error) return reject({statusCode: error.httpStatusCode, data: error});
+            return resolve({statusCode: payout.httpStatusCode, data: payout});
+        })
+    }))
+};
+
+export const createRequest = async (input: any = {}) => {
+    return new Promise((resolve, reject) => {
+        const data = <Request>input.data;
+        const method = process.env.API_METHOD || data.method || "";
+        switch (method.toLowerCase()) {
+            case "sendpayout":
+                sendPayout(<SendRequest>data)
+                    .then((response: any) => {
+                        response.data.result = response.data.batch_header.payout_batch_id || "";
+                        return resolve(response);
+                    }).catch(reject);
+                break;
+            case "getpayout":
+                getPayout(<GetRequest>data)
+                    .then((response: any) => {
+                        response.data.result = response.data.batch_header.payout_batch_id || "";
+                        return resolve(response);
+                    }).catch(reject);
+                break;
+            default:
+                return reject({statusCode: 400, data: "Invalid method"})
+        }
     })
+};
+
+export const requestWrapper = async (req: any = {}): Promise<Response> => {
+    return new Promise<Response>(resolve => {
+        let response = <Response>{jobRunID: req.id || ""};
+        createRequest(req).then(({statusCode, data}) => {
+            response.status = "success";
+            response.data = data;
+            response.statusCode = statusCode;
+            resolve(response)
+        }).catch(({statusCode, data}) => {
+            response.status = "errored";
+            response.error = data;
+            response.statusCode = statusCode;
+            resolve(response)
+        });
+    });
 };
 
 // createRequest() wrapper for GCP
 export const gcpservice = async (req: any = {}, res: any): Promise<any> => {
-    createRequest(req.body, (statusCode, data) => {
-        res.status(statusCode).send(data);
-    });
+    let response = await requestWrapper(req.body);
+    res.status(response.statusCode).send(response);
 };
 
 // createRequest() wrapper for AWS Lambda
@@ -128,7 +146,5 @@ export const handler = async (
     event: any = {},
     context: any = {},
     callback: { (error: any, result: any): void }): Promise<any> => {
-    createRequest(event, (statusCode, data) => {
-        callback(null, data);
-    });
+    callback(null, await requestWrapper(event));
 };
